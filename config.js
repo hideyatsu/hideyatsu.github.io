f9d6030a447c325c288fb4b0a361efa2d0b7ebe1
zens07 <nurzaenimuhammad@gmail.com>
2021-07-07 19:16:19 +0700

    landing page

---
+++ config.js
@@ -0,0 +1,94 @@
+const config = {
+  region1: [
+    {
+      logo: "assets/images/appilcs.png",
+      title: "eservice",
+      desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis, ad possimus eligendi quia, tempore suscipit nobis repellendus",
+      url: "www.eservice.com",
+    },
+    {
+      logo: "assets/images/appilcs.png",
+      title: "eservice",
+      desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis, ad possimus eligendi quia, tempore suscipit nobis repellendus",
+      url: "www.eservice.com",
+    },
+  ],
+  region2: [
+    {
+      logo: "assets/images/appilcs.png",
+      title: "eservice",
+      desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis, ad possimus eligendi quia, tempore suscipit nobis repellendus",
+      url: "www.eservice.com",
+    },
+    {
+      logo: "assets/images/appilcs.png",
+      title: "eservice",
+      desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis, ad possimus eligendi quia, tempore suscipit nobis repellendus",
+      url: "www.eservice.com",
+    },
+    {
+      logo: "assets/images/appilcs.png",
+      title: "eservice",
+      desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis, ad possimus eligendi quia, tempore suscipit nobis repellendus",
+      url: "www.eservice.com",
+    },
+  ],
+  region3: [
+    {
+      logo: "assets/images/appilcs.png",
+      title: "eservice",
+      desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis, ad possimus eligendi quia, tempore suscipit nobis repellendus",
+      url: "www.eservice.com",
+    },
+  ],
+  region4: [
+    {
+      logo: "assets/images/appilcs.png",
+      title: "eservice",
+      desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis, ad possimus eligendi quia, tempore suscipit nobis repellendus",
+      url: "www.eservice.com",
+    },
+    {
+      logo: "assets/images/appilcs.png",
+      title: "eservice",
+      desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis, ad possimus eligendi quia, tempore suscipit nobis repellendus",
+      url: "www.eservice.com",
+    },
+    {
+      logo: "assets/images/appilcs.png",
+      title: "eservice",
+      desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis, ad possimus eligendi quia, tempore suscipit nobis repellendus",
+      url: "www.eservice.com",
+    },
+    {
+      logo: "assets/images/appilcs.png",
+      title: "eservice",
+      desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis, ad possimus eligendi quia, tempore suscipit nobis repellendus",
+      url: "www.eservice.com",
+    },
+    {
+      logo: "assets/images/appilcs.png",
+      title: "eservice",
+      desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
+      url: "www.eservice.com",
+    },
+    {
+      logo: "assets/images/appilcs.png",
+      title: "eservice",
+      desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis, ad possimus eligendi quia, tempore suscipit nobis repellendus",
+      url: "www.eservice.com",
+    },
+    {
+      logo: "assets/images/appilcs.png",
+      title: "eservice",
+      desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis, ad possimus eligendi quia, tempore suscipit nobis repellendus",
+      url: "www.eservice.com",
+    },
+    {
+      logo: "assets/images/appilcs.png",
+      title: "eservice",
+      desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis, ad possimus eligendi quia, tempore suscipit nobis repellendus",
+      url: "www.eservice.com",
+    },
+  ],
+};
