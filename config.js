d6e28dc70a4d81b6d39ffaf27484d216978157d6
MSI\ILCS <radityopradana1@gmail.com>
2021-07-19 10:12:40 +0700

    add img

--- config.js
+++ config.js
@@ -1,37 +1,37 @@
 const config = {
   region1: [
     {
-      logo: "assets/images/not-available.png",
+      logo: "assets/images/logo-p1-white.jpg",
       title: "Webportal IGMT",
       desc: "Indonesia Gateway Master Terminal - sistem operasi terminal multipurpose yang multifungsi, mudah disesuaikan, dapat dioperasikan melalui berbagai perangkat pintar dan terintegrasi",
       url: "https://igateway.pelindo1.co.id/WebPortal/Views/LoginView/",
     },
     {
-      logo: "assets/images/not-available.png",
+      logo: "assets/images/logo-p1-white.jpg",
       title: "E-berthing",
       desc: "Sistem Aplikasi E-Berthing Online & Pelayanan Kapal Ferry",
       url: "http://e-berthing.pelindo1.co.id/tpi/",
     },
 	{
-      logo: "assets/images/not-available.png",
+      logo: "assets/images/logo-p1-white.jpg",
       title: "Portal Terminal Petikemas Belawan",
       desc: "Sistem Terminal Peti Kemas Belawan ",
       url: "http://bict.pelindo1.co.id/",
     },
 	{
-      logo: "assets/images/not-available.png",
+      logo: "assets/images/logo-p1-white.jpg",
       title: "Integrated Billing System",
       desc: "Sistem Pelayanan Billing - Receiving dan Delivery Petikemas",
       url: "https://ibs.pelindo1.co.id/IBS/",
     },
 	{
-      logo: "assets/images/not-available.png",
+      logo: "assets/images/logo-p1-white.jpg",
       title: "CRM",
       desc: "-",
       url: "",
     },
 	{
-      logo: "assets/images/not-available.png",
+      logo: "assets/images/logo-p1-white.jpg",
       title: "CDOS Perawang",
       desc: "-",
       url: "",
@@ -59,7 +59,7 @@ const config = {
   ],
   region3: [
     {
-      logo: "assets/images/not-available.png",
+      logo: "assets/images/logo-ibs-white.png",
       title: "Anjungan",
       desc: "Portal pelanggan kepelabuhanan dengan sistem yang telah terintegrasi",
       url: "https://anjungan.pelindo.co.id/",
@@ -67,37 +67,37 @@ const config = {
   ],
   region4: [
     {
-      logo: "assets/images/not-available.png",
+      logo: "assets/images/logo_new.png",
       title: "E-PPKB",
       desc: "Aplikasi PPKB Online dan Pelayanan Kapal",
       url: "https://eppkb.inaport4.co.id/",
     },
     {
-      logo: "assets/images/not-available.png",
+      logo: "assets/images/plus_logo.png",
       title: "PlusP4",
       desc: "Sistem Pelayanan Bongkar Muat Barang dan Bisnis Pendukungnya",
       url: "https://plus.inaport4.co.id/",
     },
     {
-      logo: "assets/images/not-available.png",
+      logo: "assets/images/vessp4dark.png",
       title: "VessP4",
       desc: "Sistem Pelayanan Jasa Kapal",
       url: "https://vessp4.inaport4.co.id/",
     },
     {
-      logo: "assets/images/not-available.png",
+      logo: "assets/images/logo_new.png",
       title: "Drone",
       desc: "Sistem Pelayanan Terminal Petikemas Makasar",
       url: "http://drone.tpk-mks.co.id/",
     },
     {
-      logo: "assets/images/not-available.png",
+      logo: "assets/images/p4ibs.PNG",
       title: "Integrated Billing System",
       desc: "Sistem Billing yang terintegrasi ke Seluruh Pelayanan Operasional Pelabuhan",
       url: "http://e-billing.inaport4.co.id",
     },
     {
-      logo: "assets/images/not-available.png",
+      logo: "assets/images/img2.png",
       title: "MyPort - Inapos",
       desc: "Integrated & Analytic Problem Solved",
       url: "https://myport.inaport4.co.id/",
